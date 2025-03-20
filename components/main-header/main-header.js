6a25c3a96180e52c9061241f9e54528fb8459ecf
Void-404X <alisherwashere@gmail.com>
2025-03-20 15:58:22 +0800

    Add NavBar

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,12 +1,23 @@
+<<<<<<< HEAD
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
+=======
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import MainHeaderBackground from "./main-header.background";
+import NavLink from "./nav-link";
+>>>>>>> 67bd09c (Add navbar)
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 
 const MainHeader = () => {
   return (
+<<<<<<< HEAD
     <header className={classes.header}>
       <Link className={classes.logo} href="/">
         <Image src={logoImg} alt="A plate with food on it" />
@@ -24,6 +35,28 @@ const MainHeader = () => {
         </ul>
       </nav>
     </header>
+=======
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals">Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community">Foodies Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+>>>>>>> 67bd09c (Add navbar)
   );
 };
 
